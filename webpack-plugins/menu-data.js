0103997166a4b709804156c7760ea6082dbb8745
Collin <candrew022@gmail.com>
2018-05-08 11:16:28 -0400

    Seperate data from UI

---
+++ webpack-plugins/menu-data.js
@@ -0,0 +1,366 @@
+const menuData = [
+  {
+    title: "Home",
+    href:"internal.html",
+    children: null
+  },
+  {
+    title: "About",
+    href:"internal.html",
+    children: [
+      {
+        title: "Firm Profile",
+        href:"internal.html",
+        children: null
+      },
+      {
+        title: "Employment Opportunities",
+        href:"internal.html",
+        children: null
+      },
+      {
+        title: "Blog",
+        href:"internal.html",
+        children: null
+      },
+    ]
+  },
+  {
+    title: "Services",
+    href:"internal.html",
+    children: [
+      {
+        title: "Tax Services",
+        href:"internal.html",
+        children: [
+          {
+            title: "Tax Preparation",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Tax Planning",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Tax Problems",
+            href:"internal.html",
+            children: null
+          },
+        ]
+      },
+      {
+        title: "Business Services",
+        href:"internal.html",
+        children: [
+          {
+            title: "Small Business Accounting",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Payroll",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Part-Time CFO Services",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Audits - Reviews - Compilations",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Forensic Accounting",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Cash Flow Management",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Bank Financing",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Business Valuation",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Strategic Business Planning",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Succession Planning",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "New Business Formation",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Non-Profit Organization",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Internal Controls",
+            href:"internal.html",
+            children: null
+          },
+        ]
+      },
+      {
+        title: "Quickbooks Services",
+        href:"internal.html",
+        children: [
+          {
+            title: "Why Quickbooks",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Quickbooks Setup",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Quickbooks Training",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "QuickAnswers",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "QuickTune-up",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Buy Quickbooks and Save",
+            href:"internal.html",
+            children: null
+          },
+        ]
+      },
+      {
+        title: "Services for Individuals",
+        href:"internal.html",
+        children: [
+          {
+            title: "Personal Financial Planning",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Estate Planning",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Elder Care",
+            href:"internal.html",
+            children: null
+          },
+        ]
+      },
+      {
+        title: "Industries",
+        href:"internal.html",
+        children: [
+          {
+            title: "Construction",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Dentists",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Healthcare",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Hospitality",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Law Firms",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Manufacturers",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Real Estate",
+            href:"internal.html",
+            children: null
+          },
+        ]
+      },
+    ]
+  },
+  {
+    title: "Client Center",
+    href:"internal.html",
+    children: [
+      {
+        title: "Newsletter",
+        href:"internal.html",
+        children: [
+          {
+            title: "This Month's Newsletter",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Previous Newsletter",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Today's News and Weather",
+            href:"internal.html",
+            children: null
+          },
+        ]
+      },
+      {
+        title: "Financial Guides",
+        href:"internal.html",
+        children: [
+          {
+            title: "Life Events",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Business Strategies",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Investment Strategies",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Tax Strategies for Business Owners",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Tax Strategies for Individuals",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Frequently Asked Questions",
+            href:"internal.html",
+            children: null
+          },
+        ]
+      },
+      {
+        title: "Tax Center",
+        href:"internal.html",
+        children: [
+          {
+            title: "Track Your Refund",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Tax Dues Dates",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Tax Rates",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "IRS Tax Forms and Publications",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Record Retention",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "State Tax Forms",
+            href:"internal.html",
+            children: null
+          },
+        ]
+      },
+      {
+        title: "Resources",
+        href:"internal.html",
+        children: [
+          {
+            title: "Financial Calculators",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Client Portal",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "SecureSend",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "Internet Links",
+            href:"internal.html",
+            children: null
+          },
+          {
+            title: "News and Weather",
+            href:"internal.html",
+            children: null
+          },
+        ]
+      },
+    ]
+  },
+  {
+    title: "Reviews",
+    href:"internal.html",
+    children: null
+  },
+  {
+    title: "Contact",
+    href:"internal.html",
+    children: null
+  },
+]
+
+module.exports = menuData;
--- webpack-plugins/smart-menu.js
+++ webpack-plugins/smart-menu.js
@@ -1,126 +1,23 @@
-const smartMenu = `<ul class="nav navbar-nav">
-  <li><a href="internal.html" target="_top">Home</a></li>
-  <li>
-    <a href="internal.html" target="_top">About</a>
-    <ul class="dropdown-menu">
-      <li>
-        <a href="internal.html" target="_top">Firm Profile</a>
-      </li>
-      <li>
-        <a href="internal.html" target="_top">Employment Opportunities</a>
-      </li>
-      <li>
-        <a href="internal.html" target="_top">Blog</a>
-      </li>
-    </ul>
-  </li>
-  <li>
-    <a href="internal.html" target="_top">Services</a>
-    <ul class="dropdown-menu">
-      <li>
-        <a href="internal.html" target="_top">Tax Services</a>
-      </li>
-      <li>
-        <a href="internal.html" target="_top">Business Services</a>
-      </li>
-      <li>
-        <a href="internal.html" target="_top">Quickbooks Services</a>
-      </li>
-      <li>
-        <a href="internal.html" target="_top">Services for Individuals</a>
-      </li>
-      <li>
-        <a href="internal.html" target="_top">Industries</a>
-      </li>
-    </ul>
-  </li>
-  <li>
-    <a href="internal.html" target="_top">Client Center</a>
-    <ul class="dropdown-menu">
-      <li>
-        <a href="internal.html" target="_top">Newsletter</a>
-        <ul class="dropdown-menu">
-          <li>
-            <a href="internal.html" target="_top">This Month's Newsletter</a>
-          </li>
-          <li>
-            <a href="internal.html" target="_top">Previous Newsletter</a>
-          </li>
-          <li>
-            <a href="internal.html" target="_top">Today's News and Weather</a>
-          </li>
-        </ul>
-      </li>
-      <li>
-        <a href="internal.html" target="_top">Financial Guides</a>
-        <ul class="dropdown-menu">
-          <li>
-            <a href="internal.html" target="_top">Life Events</a>
-          </li>
-          <li>
-            <a href="internal.html" target="_top">Business Strategies</a>
-          </li>
-          <li>
-            <a href="internal.html" target="_top">Investment Strategies</a>
-          </li>
-          <li>
-            <a href="internal.html" target="_top">Tax Strategies for Business Owners</a>
-          </li>
-          <li>
-            <a href="internal.html" target="_top">Tax Strategies for Individuals</a>
-          </li>
-          <li>
-            <a href="internal.html" target="_top">Frequently Asked Questions</a>
-          </li>
-        </ul>
-      </li>
-      <li>
-        <a href="internal.html" target="_top">Tax Center</a>
-        <ul class="dropdown-menu">
-          <li>
-            <a href="internal.html" target="_top">Track Your Refund</a>
-          </li>
-          <li>
-            <a href="internal.html" target="_top">Tax Dues Dates</a>
-          </li>
-          <li>
-            <a href="internal.html" target="_top">Tax Rates</a>
-          </li>
-          <li>
-            <a href="internal.html" target="_top">IRS Tax Forms and Publications</a>
-          </li>
-          <li>
-            <a href="internal.html" target="_top">Record Retention</a>
-          </li>
-          <li>
-            <a href="internal.html" target="_top">State Tax Forms</a>
-          </li>
-        </ul>
-      </li>
-      <li>
-        <a href="internal.html" target="_top">Resources</a>
-        <ul class="dropdown-menu">
-          <li>
-            <a href="internal.html" target="_top">Financial Calculators</a>
-          </li>
-          <li>
-            <a href="internal.html" target="_top">Client Portal</a>
-          </li>
-          <li>
-            <a href="internal.html" target="_top">SecureSend</a>
-          </li>
-          <li>
-            <a href="internal.html" target="_top">Internet Links</a>
-          </li>
-          <li>
-            <a href="internal.html" target="_top">News and Weather</a>
-          </li>
-        </ul>
-      </li>
-    </ul>
-  </li>
-  <li><a href="internal.html" target="_top">Reviews</a></li>
-  <li><a href="internal.html" target="_top">Contact</a></li>
-</ul>`
+const menuData = require('./menu-data');
+
+let smartMenu = '<ul class="nav navbar-nav">'
+
+const buildMenu = (list) => (
+  list.map((item) => {
+    let itemString = `<li><a href="${item.href}" target="_top">${item.title}</a>`
+    if (item.children) {
+      itemString += `<ul class="dropdown-menu">`
+      itemString += buildMenu(item.children)
+      itemString += `</ul>`
+    }
+    itemString += `</li>`
+    return itemString;
+  }).join('')
+)
+
+smartMenu += buildMenu(menuData);
+
+smartMenu += '</ul>'
+
 
 module.exports = smartMenu;
