02ca612e691b7053d63038eeb59f97eb45e52c2d
Collin <candrew022@gmail.com>
2018-05-08 11:53:19 -0400

    Add background url image pathing to replacement

--- webpack-plugins/replace-config/pathing.js
+++ webpack-plugins/replace-config/pathing.js
@@ -1,10 +1,10 @@
 const replacePathing = [
   {
-    pattern: /(src="|href=")\/(images|~)/,
+    pattern: /(src="|href="|url\()\/(images|~)/,
     replacement: function (match, protocol, path) {
       return `${protocol}http://cp${process.env.CPSERVER}.cpasitesolutions.com/${path}`
     }
-  }
+  },
 ]
 
-module.exports = replacePathing
+module.exports = replacePathing;
