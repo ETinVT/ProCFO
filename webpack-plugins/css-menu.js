b596d92fbdc21574dbbefc0b91feaf419ee0f77b
Collin <candrew022@gmail.com>
2018-05-09 10:47:35 -0400

    varry links

--- webpack-plugins/css-menu.js
+++ webpack-plugins/css-menu.js
@@ -8,7 +8,7 @@ const buildMenu = (list) => (
       <li ${item.children ? 'class="parent"' : null}>
         <span class="${item.children ? 'topdaddy' : 'top'}">
           <span class="leftBG"></span>
-          <a href="${item.href}" target="_top">${item.title}</a>
+          <a href="${item.href}?${Math.random()}" target="_top">${item.title}</a>
           <span class="rightBG"></span>
           <span class="selector"></span>
         </span>`
--- webpack-plugins/smart-menu.js
+++ webpack-plugins/smart-menu.js
@@ -4,7 +4,7 @@ let smartMenu = '<ul class="nav navbar-nav">'
 
 const buildMenu = (list) => (
   list.map((item) => {
-    let itemString = `<li><a href="${item.href}" target="_top">${item.title}</a>`
+    let itemString = `<li><a href="${item.href}?${Math.random()}" target="_top">${item.title}</a>`
     if (item.children) {
       itemString += `<ul class="dropdown-menu">`
       itemString += buildMenu(item.children)
