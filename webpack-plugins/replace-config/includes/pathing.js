7952fa3eb8b33d5477c02ee0f4fa871136e885f4
Collin <candrew022@gmail.com>
2018-05-09 14:16:13 -0400

    Add conditional pathing based on env variables

--- webpack-plugins/replace-config/includes/pathing.js
+++ webpack-plugins/replace-config/includes/pathing.js
@@ -1,6 +1,18 @@
 const pathingReplacement = [
   {
-    pattern: /(src="|href="|url\()\/(images|~)/,
+    pattern: process.env.CSSLOCATION === 'local' ? /skipreplacement/ : /(href=")\/(images|~)/,
+    replacement: function (match, protocol, path) {
+      return `${protocol}http://cp${process.env.CPSERVER}.cpasitesolutions.com/${path}`
+    }
+  },
+  {
+    pattern: process.env.JSLOCATION === 'local' ? /skipreplacement/ : /(src=")\/(images|~)/,
+    replacement: function (match, protocol, path) {
+      return `${protocol}http://cp${process.env.CPSERVER}.cpasitesolutions.com/${path}`
+    }
+  },
+  {
+    pattern: process.env.IMGLOCATION === 'local' ? /skipreplacement/ : /(url\()\/(images|~)/,
     replacement: function (match, protocol, path) {
       return `${protocol}http://cp${process.env.CPSERVER}.cpasitesolutions.com/${path}`
     }
